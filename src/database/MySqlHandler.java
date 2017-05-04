095603c9b0c00052e951d11579c83266daea24c1
elev <elev@192.168.1.66>
2017-05-04 15:58:21 +0200

    Changes to item.java and mysqlhandler.java

--- src/database/MySqlHandler.java
+++ src/database/MySqlHandler.java
@@ -1,10 +1,75 @@
-/**
- * 
- */
 package database;
 
-/**
- * @author Gustaf Peter Hultgren **/
-public class MySqlHandler {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import store.Item;
+
+/**
+ * @author Samuel Häggström **/
+public class MySqlHandler {
+    
+    private static final String DB_URL = "jdbc:mysql://localhost:3306";
+
+    private static final String DB_DATABASE = "item_web_shop";
+
+    private static final String DB_USERNAME = "root";
+
+    private static final String DB_PASSWORD = "root";
+
+    private static Connection cn;
+
+    public static final void connect() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        cn = DriverManager.getConnection(DB_URL + "/" + DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+    }
+
+    public static final Connection getConnection() throws SQLException, ClassNotFoundException {
+        if (cn == null) {
+            connect();
+        }
+
+        return cn;
+    }
+
+    public static final boolean close() throws SQLException {
+        if (cn != null) {
+            cn.close();
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public List<Item> getItems() throws SQLException {
+        List<Item> itemList = new ArrayList<>();
+        
+        String sql = "SELECT * FROM item";
+        
+        Statement stmt = cn.createStatement();
+        stmt.closeOnCompletion();
+        
+        ResultSet rs = stmt.executeQuery(sql);
+        
+        while (rs.next()) {
+            Item item = new Item(
+                    rs.getString("category"),
+                    rs.getString("item_name"),
+                    rs.getString("art_number"),
+                    rs.getFloat("price"),
+                    rs.getString("description"),
+                    rs.getBlob("image"),
+                    rs.getInt("stock_balance"));
+            
+            itemList.add(item);
+        }
+        
+        return itemList;
+    }
+    
 }
--- src/store/Item.java
+++ src/store/Item.java
@@ -3,8 +3,85 @@
  */
 package store;
 
+import java.sql.Blob;
+
 /**
- * @author Gustaf Peter Hultgren **/
+ * @author Samuel Häggström **/
 public class Item {
+    
+    private String category;
+    private String itemName;
+    private String artNr;
+    private float price;
+    private String description;
+    private Blob image;
+    private int stockBalance;
+    
+    public Item(String category, String itemName, String artNr, float price,
+            String description, Blob image, int stockBalance) {
+        this.category = category;
+        this.itemName = itemName;
+        this.artNr = artNr;
+        this.price = price;
+        this.description = description;
+        this.image = image;
+        this.stockBalance = stockBalance;
+    }
+    
+    public String getCategory() {
+        return category;
+    }
+    
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    public String getItemName() {
+        return itemName;
+    }
+    
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+    
+    public String getArtNr() {
+        return artNr;
+    }
+    
+    public void setArtNr(String artNr) {
+        this.artNr = artNr;
+    }
+    
+    public float getPrice() {
+        return price;
+    }
+    
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public Blob getImage() {
+        return image;
+    }
+    
+    public void setImage(Blob image) {
+        this.image = image;
+    }
+    
+    public int getStockBalance() {
+        return stockBalance;
+    }
+    
+    public void setStockBalance(int stockBalance) {
+        this.stockBalance = stockBalance;
+    }
 
 }
