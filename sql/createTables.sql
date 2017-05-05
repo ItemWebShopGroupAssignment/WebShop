20011082f807547b3ec9816139e89d1349f9ed80
elev <elev@192.168.1.123>
2017-05-05 09:39:58 +0200

    +items

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -8,12 +8,32 @@ CREATE TABLE IF NOT EXISTS `categories`(
     PRIMARY KEY(category_name)
 ) ENGINE=INNODB;
 
-CREATE TABLE `shopping_carts`(
+CREATE TABLE IF NOT EXISTS `items` (
+	`art_number` VARCHAR(50) NOT NULL,
+	`item_name` VARCHAR(50) NOT NULL,
+	`price` FLOAT NOT NULL,
+	`description` TEXT NULL,
+	`image` BLOB NULL,
+	`stock_balance` INT(10) UNSIGNED NOT NULL,
+	`storage_formats` VARCHAR(50) NOT NULL,
+	`category` VARCHAR(50) NOT NULL,
+	PRIMARY KEY (`art_number`),
+	INDEX `category_idx` (`category`),
+	CONSTRAINT `category` 
+	FOREIGN KEY (`category`) 
+	REFERENCES `categories` (`category_name`) 
+	ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+COLLATE='utf8_swedish_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE IF NOT EXISTS `shopping_carts`(
 	cart_id INT(5) AUTO_INCREMENT,
     PRIMARY KEY(cart_id)
 ) ENGINE=INNODB;
 
-CREATE TABLE `cart_items` (
+CREATE TABLE IF NOT EXISTS `cart_items` (
 	art_number VARCHAR(50) NOT NULL,
     item_name VARCHAR(50) NOT NULL,
     price FLOAT(8,2) NOT NULL,
