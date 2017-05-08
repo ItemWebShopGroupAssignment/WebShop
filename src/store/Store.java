4a343d92f923569df82d24fe60a82c4165710558
Robert Fängström <Robert-Fang@github.com>
2017-05-08 14:04:24 +0200

    Updated code

--- src/store/Store.java
+++ src/store/Store.java
@@ -11,55 +11,55 @@ public class Store {
 
 	private MySqlHandler storeHandler;
 
-	public List<Item> showInventory() throws SQLException {
+	public List<Item> showInventory() throws SQLException, ClassNotFoundException {
 		return storeHandler.getItems();
 
 	}
 
-	public Item getItem(String artNr) throws SQLException {
+	public Item getItem(String artNr) throws SQLException, ClassNotFoundException {
 		return storeHandler.getItem(artNr);
 	}
 
-	public boolean addToCart(String artNr, int cartId, int count) throws SQLException {
+	public boolean addToCart(String artNr, int cartId, int count) throws SQLException, ClassNotFoundException {
 		return storeHandler.addToCart(cartId, artNr, count);
 	}
 
-	public boolean addToInventory(String artNr, int count) throws SQLException {
+	public boolean addToInventory(String artNr, int count) throws SQLException, ClassNotFoundException{
 		return storeHandler.addToInventory(artNr, count);
 	}
 
-	public boolean insertToInventory(Item item) throws SQLException {
+	public boolean insertToInventory(Item item) throws SQLException, ClassNotFoundException {
 		return storeHandler.insertItemToInventory(item);
 	}
 
-	public List<String> showCategories() throws SQLException {
+	public List<String> showCategories() throws SQLException, ClassNotFoundException {
 		return storeHandler.showCategories();
 	}
 
-	public boolean returnFromCart(String artNr, int cartId, int count) throws SQLException {
+	public boolean returnFromCart(String artNr, int cartId, int count) throws SQLException, ClassNotFoundException {
 		return storeHandler.returnFromCart(cartId, artNr, count);
 	}
 
-	public List<String> checkOutCart(int cartId) throws SQLException {
+	public List<String> checkOutCart(int cartId) throws SQLException, ClassNotFoundException {
 		return storeHandler.checkOutCart(cartId);
 	}
 
-	public boolean deleteFromInventory(String artNr, int cartId) throws SQLException {
+	public boolean deleteFromInventory(String artNr, int cartId) throws SQLException, ClassNotFoundException {
 		return storeHandler.deleteFromInventory(artNr);
 	}
 
-	public boolean dumpCart(int cartId) throws SQLException {
+	public boolean dumpCart(int cartId) throws SQLException, ClassNotFoundException {
 
 		return storeHandler.dumpCart(cartId);
 
 	}
 
-	public boolean subtractFromInventory(String artNr, int count) throws SQLException {
+	public boolean subtractFromInventory(String artNr, int count) throws SQLException, ClassNotFoundException {
 		return storeHandler.subtractFromInventory(artNr, count);
 
 	}
 	
-	public List<Item> getCartItems(int cartId) throws SQLException {
+	public List<Item> getCartItems(int cartId) throws SQLException, ClassNotFoundException {
 		return storeHandler.getCartItems(cartId);
 	}
 
