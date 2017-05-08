239b92bb3e6b57edccca068291ffd193c89467d2
elev <elev@192.168.1.123>
2017-05-08 14:24:59 +0200

    Routing och ful Browse

--- WebContent/script/routing.js
+++ WebContent/script/routing.js
@@ -30,7 +30,43 @@
 		
 	};
 	
-	var browseController = function($scope) {
+	var browseController = function($scope, $http) {
+//		routingApplication.controller("booksController", function($scope, $http) {
+			$scope.title = "Items";
+			
+			var items = [];
+			$scope.items = items;
+
+			$scope.removeFromInventory = function(index) {
+				
+				//här tar vi bort spelet från listan:
+				$scope.items.splice(index, 1); 
+				
+				//Det som återstår är att ta bort spelet i databasen, 
+				//och tillhörande funktionalitet för det.
+			};
+
+			var onItemsComplete = function(response) {
+				$scope.items = response.data;
+			}
+
+//			var onAddItemComplete = function(response) {
+//				$scope.description = "Added new game to list " + response.data;
+//				
+//				//här lägger vi till spelet i listan:
+//				var tmp = angular.copy(item);
+//				$scope.items.push(tmp);
+//				$scope.item.title = "";
+//				$scope.item.author = "";
+//
+//			}
+
+			var onError = function(reason) {
+				$scope.error = "Could not fetch data " + reason.status;
+			}
+
+			$http.get("GetItems").then(onItemsComplete, onError);
+	
 		
 	};
 	
@@ -38,6 +74,6 @@
 	app.config(["$routeProvider", config]);
 	app.controller("homeController", ["$scope", homeController]);
 	app.controller("cartController", ["$scope", cartController]);
-	app.controller("browseController", ["$scope", browseController]);
+	app.controller("browseController", ["$scope", "$http", browseController]);
 	
 }())
