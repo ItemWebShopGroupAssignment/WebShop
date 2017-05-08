2e9060c06454f21374b9c211a53ee6a7bbc70d02
elev <elev@192.168.1.66>
2017-05-08 13:20:14 +0200

    Added GetImageServlet

---
+++ src/web/GetImageServlet.java
@@ -0,0 +1,61 @@
+package web;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import database.MySqlHandler;
+import store.Item;
+
+/**
+ * Servlet implementation class GetImageServlet
+ */
+@WebServlet("/GetImage")
+public class GetImageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetImageServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    String artNr = request.getParameter("artNr");
+        MySqlHandler db = new MySqlHandler();
+        Item item;
+        try {
+            item = db.getItem(artNr);
+            Blob blob = item.getImage();
+            byte[] image = blob.getBytes(1, (int) blob.length());
+            OutputStream output = response.getOutputStream();
+            response.setContentType("image/gif");
+            output.write(image);
+            output.flush();
+            output.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
