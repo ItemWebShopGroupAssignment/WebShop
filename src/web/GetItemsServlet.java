f73bd98883a21b4cefc1df088dee49815ebaeef7
Draelgar <g.peter.hultgren@gmail.com>
2017-05-08 11:38:15 +0200

    Added get inventory items servlet.

--- src/web/GetItemsServlet.java
+++ src/web/GetItemsServlet.java
@@ -1,11 +1,20 @@
 package web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import store.Item;
+import store.Store;
 
 /**
  * Servlet implementation class GetItemsServlet
@@ -26,15 +35,26 @@ public class GetItemsServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		Store store = new Store();
+		
+		try {
+			List<Item> inventory = store.showInventory();
+			
+			response.setContentType("application/json;characterset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.print(new Gson().toJson(inventory));
+			out.flush();
+			
+		} catch (SQLException e) {
+			response.getWriter().append("Error: " + e.getMessage());
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
