60affb9d6dfa78700e262714a1b084c08f4e5fa3
elev <elev@192.168.1.66>
2017-05-09 11:12:25 +0200

    New dummy data

---
+++ sql/-updatedDummyData.sql
@@ -0,0 +1,8 @@
+INSERT INTO `items` VALUES ('art:212-4527BE','Dishonored 2',599.00,'Reprise your role as a supernatural assassin in Dishonored 2.',NULL,5,'PC Download','Action'),
+('art:422-4527BA','Civilization III',499.00,'The perfect strategy game.',NULL,10,'PC','TB'),
+('art:511-4527EA','The Division',699.00,'Black Friday – a devastating pandemic sweeps through New York City, and one by one, basic services fail. In only days, without food or water, society collapses into chaos. The Division, an autonomous unit of tactical agents, is activated.',NULL,8,'PS5','Action'),
+('art:512-4527EA','BioShock Infinite',699.00,'Indebted to the wrong people, with his life on the line, veteran of the U.S. Cavalry and now hired gun, Booker DeWitt has only one opportunity to wipe his slate clean. He must rescue Elizabeth, a mysterious girl imprisoned since childhood and locked up in the flying city of Columbia.',NULL,7,'PS5','Adventure'),
+('art:532-4527EA','Little Nightmares',599.00,'Immerse yourself in Little Nightmares, a dark whimsical tale that will confront you with your childhood fears! Help Six escape The Maw – a vast, mysterious vessel inhabited by corrupted souls looking for their next meal.',NULL,10,'PC HDDVD','Horror'),
+('art:533-4527FI','Grand Theft Auto V',699.00,'A young street hustler, a retired bank robber and a terrifying psychopath must pull off a series of dangerous heists to survive in a ruthless city in which they can trust nobody, least of all each other.',NULL,13,'PC','Action'),
+('art:544-4527EA','FOR HONOR',599.00,'Carve a path of destruction through an intense, believable battlefield in For Honor.',NULL,11,'PC Download','Action'),
+('art:572-4527BE','World of Warcraft',599.00,'A game about a fantasy world.',NULL,10,'PC Download','MMORPG');
