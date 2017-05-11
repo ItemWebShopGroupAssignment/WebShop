dd19f1d033da7a9371b1bab0aaa6f23423505537
elev <elev@192.168.1.66>
2017-05-11 15:09:28 +0200

    Updates

--- src/web/CheckoutCartServlet.java
+++ src/web/CheckoutCartServlet.java
@@ -1,11 +1,21 @@
 package web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import store.Item;
+import store.Store;
 
 /**
  * Servlet implementation class CheckoutCartServlet
@@ -26,7 +36,36 @@ public class CheckoutCartServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    
+	    request.setCharacterEncoding("UTF-8");
+        Store store = new Store();
+        
+        try {
+            // Get the added data.
+            String data = request.getReader().lines().collect(Collectors.joining());
+            Item item = new Gson().fromJson(data, Item.class); // Retrieve the item from the data.
+            System.out.println(data);
+            if(item != null && item.getCartId() >= 0) {
+                List<String> result = store.checkOutCart(item.getCartId());
+                
+                response.setContentType("application/json;characterset=UTF-8");
+                PrintWriter out = response.getWriter();
+                
+                if(result.size() > 0)
+                    out.print("Successfully checked out the cart.");
+                else
+                    out.print("Failed to check out the cart.");
+                
+                out.flush();
+            }
+            else {
+                response.getWriter().append("Warning: No parameters available!");
+            }
+            
+        } catch (SQLException e) {
+            response.getWriter().append("Error: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            response.getWriter().append("Error: " + e.getMessage());
+        }
 	}
 
 }
