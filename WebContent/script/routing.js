ad6d3dd4c059df23d209ec99962d003a2d6a10f6
Draelgar <g.peter.hultgren@gmail.com>
2017-05-12 09:30:51 +0200

    Fixed support for carts.
    Now the cart is not asked for until the first time the customer tries to
    add a new product to the cart.

--- WebContent/script/routing.js
+++ WebContent/script/routing.js
@@ -4,7 +4,7 @@
 
 (function() {
 	
-	var cartId = 0;
+	var cartId = -1;
 	
 	var app = angular.module("application", [ "ngRoute" ]);
 
@@ -29,17 +29,7 @@
 		});
 	};
 
-	var homeController = function($scope, $http) {
-			
-		$http.get("GetCart").then(onIdFetchComplete, onError);
-		
-		var onIdFetchComplete = function(response) {
-			cartId = response.data;
-		}
-		
-		var onError = function(reason) {
-			alert(reason.status);
-		}
+	var homeController = function($scope) {
 		
 	};
 
@@ -96,6 +86,7 @@
 			
 			$http.post("CheckoutCart", JSON.stringify(parameter)).then(onCheckoutComplete, onCartError);
 		}
+		
 	};
 
 	var browseController = function($scope, $http) {
@@ -136,6 +127,10 @@
 		}
 		
 		$scope.addToCart = function(artNr, count, stockBalance) {
+			if(cartId == -1) {
+				$http.get("GetCart").then(onIdFetchComplete, onError);
+			}
+			
 			if (stockBalance <= 0) {
 				alert("Not in stock!");
 			}
@@ -150,6 +145,14 @@
 				
 				$http.post("AddToCart", jsonPackage).then(onAddToCartComplete, onError);
 			}
+		}
+		
+		var onIdFetchComplete = function(response) {
+			cartId = response.data;
+		}
+		
+		var onError = function(reason) {
+			alert(reason.status);
 		}
 
 	};
