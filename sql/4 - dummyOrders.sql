53c200e9d3cf9587587158a01be3aa4eb12f54ab
Draelgar <g.peter.hultgren@gmail.com>
2017-05-15 15:20:17 +0200

    Added dummy data for orders.

---
+++ sql/4 - dummyOrders.sql
@@ -0,0 +1,9 @@
+USE item_web_shop;
+
+INSERT INTO orders
+VALUES(NULL, 29.0, 728.0, FALSE, 'Some adress', 'John Smith');
+INSERT INTO orders
+VALUES(NULL, 29.0, 928.0, FALSE, 'Some other adress', 'John Doe');
+INSERT INTO orders
+VALUES(NULL, 29.0, 1328.0, FALSE, 'Shady adress', 'Mr. Shady');
+
