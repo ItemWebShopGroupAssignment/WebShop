77870400b8415e596af807adedcaa0284087f6ee
Draelgar <g.peter.hultgren@gmail.com>
2017-05-15 13:37:50 +0200

    Comments

--- WebContent/script/browseController.js
+++ WebContent/script/browseController.js
@@ -4,37 +4,39 @@
 
 (function() {
 	
+	// Get the angular module created in routing.js.
 	var app = angular.module("application");
 	
 	var browseController = function($scope, $http, $timeout) {
+		/* Get the item list. */
+		$http.get("GetItems").then(onItemsComplete, onError);
+		
 		$scope.title = "";
+		$scope.hideDescription = true;
 
-		var items = [];
-		$scope.items = items;
-
+		/* Called when the get items request receives a correct response. */
 		var onItemsComplete = function(response) {
 			$scope.items = response.data;
 		}
 
-		 var onAddToCartComplete = function(response) {
-			 $scope.title = response.data;
+		/* Called the call to AddToCart returns a correct response. */
+		var onAddToCartComplete = function(response) {
+			// Show a message for 3 seconds. 
+			$scope.title = response.data;
+			$timeout(function() {
+				$scope.title = "";
+			}, 3000);
 			 
-			 $timeout(function() {
-				 $scope.title = "";
-			 }, 3000);
-			 
-			 $http.get("GetItems").then(onItemsComplete, onError);
+			$http.get("GetItems").then(onItemsComplete, onError);
 
-		 }
+		}
 
+		/* Called when a HTTP error occurs. */
 		var onError = function(reason) {
 			alert("Could not fetch data " + reason.status);
 		}
-
-		$http.get("GetItems").then(onItemsComplete, onError);
-
-		$scope.hideDescription = true;
 		
+		/* Called when an item in the list is clicked. */
 		$scope.viewDescription = function(id) {
 			for(var i = 0; i < $scope.items.length; i++) {
 				if(id === $scope.items[i].artNr) {
@@ -45,6 +47,7 @@
 			}
 		}
 		
+		/* Called when an item is to be added to the cart. */
 		$scope.addToCart = function(artNr, count, stockBalance) {			
 			if (stockBalance <= 0) {
 				alert("Not in stock!");
@@ -63,6 +66,7 @@
 
 	};
 	
+	// Bind the controller.
 	app.controller("browseController", [ "$scope", "$http", "$timeout", browseController ]);
 	
 }())
--- WebContent/script/cartController.js
+++ WebContent/script/cartController.js
@@ -4,6 +4,7 @@
 
 (function() {
 	
+	// Get the angular module created in routing.js.
 	var app = angular.module("application");
 		
 	var cartController = function($scope, $http) {
@@ -13,6 +14,10 @@
 		$scope.showTableHeaders = false;
 		$scope.totalCost = 0;
 		
+		// Call the back-end and ask for the item list for the cart.
+		$http.get("GetCartItems").then(onGetCartComplete, onCartError);
+		
+		/* Called when a list of items is returned. */
 		var onGetCartComplete = function(response) {
 
 			$scope.items = response.data;
@@ -34,12 +39,12 @@
 
 		}
 		
+		// Called when a HTTP error has occured.
 		var onCartError = function(reason) {
 			alert(reason.status);
 		}
 
-		$http.get("GetCartItems").then(onGetCartComplete, onCartError);
-
+		/* Called when a remove from cart call responds correctly. */
 		var onRemoveComplete = function(response) {
 			$http.get("GetCartItems?").then(onGetCartComplete, onCartError);
 			
@@ -47,6 +52,7 @@
 			$scope.selectedItem = null;
 		}
 		
+		/* Called every time one of the remove from cart buttons are pressed. */
 		$scope.removeFromCart = function(artNr, count){
 			var parameters = {
 					'artNr' : artNr,
@@ -58,10 +64,12 @@
 			$http.post("RemoveFromCart", jsonParameters).then(onRemoveComplete, onCartError);
 		}
 		
+		/* Called when dump cart button is pressed. */
 		$scope.dumpCart = function() {
 			 $http.post("DumpCart").then(onDumpedComplete, onCartError);
 		}
 		
+		/* Called when the call to DumpCart responds with the correct response. */
 		var onDumpedComplete = function(response) {
 			/* Update list and info. */
 			$scope.items = [];
@@ -73,6 +81,7 @@
 			$scope.selectedItem = null;
 		}
 		
+		/* Called when the call to Checkout responds with the correct response. */
 		var onCheckoutComplete = function(response) {
 			/* Update list and info. */
 			$scope.items = [];
@@ -84,17 +93,20 @@
 			$scope.selectedItem = null;
 		}
 		
+		/* Called when checkout cart button is pressed. */
 		$scope.checkoutCart = function() {
 			
 			$http.post("CheckoutCart").then(onCheckoutComplete, onCartError);
 		}
 		
+		/* Called when an item in the list is clicked. */
 		$scope.showInfo = function(index) {
 			$scope.hideDescription = false;
 			$scope.selectedItem = $scope.items[index];
 		}
 	};
 	
+	// Bind the controller.
 	app.controller("cartController", [ "$scope", "$http", cartController ]);
 	
 }())
